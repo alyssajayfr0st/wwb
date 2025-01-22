3e3512fdf8c28bf1926d325714ad8a0a7fb0c750
alyssajay <alysjay@proton.me>
2025-01-21 21:45:00 -0700

    adds some comments to slightly reduce the suffuring of anyone that reads this pile of code. improves performance considerably by changing where some things ran and removing an if statement. also changes the layout to something a bit less insane

--- orchestra.cpp
+++ orchestra.cpp
@@ -6,27 +6,38 @@
 #include <iomanip>
 using namespace std;
 using namespace XoshiroCpp;
-        bool rezero = false;
-        int roll = 0;
-        int space = 0;
-        int highest = 0;
-        int rollcall = 0;
-        int highroll = 0;
-        double totalroll = 0;
-        unsigned long rps = 0;
-        unsigned long cps = 0;
-        bool end = false;
-        bool ctrlCPressed = false;
-        long endcount = 0;
-        const int startNum = 1;
-        const int endNum = 6;
-        double rollcount = 0;
-        double guage;
-        unsigned long timewasted = 0;
-        random_device rd;
-        uint64_t seed = rd();
-        Xoshiro256PlusPlus rng(seed);
-        uniform_int_distribution<> dist(1,6);
+// initialization and variable assignment
+bool rezero = false;
+int roll = 0;
+int space = 0;
+int highest = 0;
+int rollcall = 0;
+int highroll = 0;
+double totalroll = 0;
+unsigned long rps = 0;
+unsigned long cps = 0;
+bool end = false;
+bool ctrlCPressed = false;
+long endcount = 0;
+const int startNum = 1;
+const int endNum = 6;
+double rollcount = 0;
+double guage;
+unsigned long timewasted = 0;
+unsigned long minuteswasted = 0;
+unsigned long hourswasted = 0;
+bool running = true;
+long iteration = 0;
+double seconds;
+bool loop = true;
+bool timerset;
+double timerlength;
+double timerrun;
+random_device rd;
+uint64_t seed = rd();
+Xoshiro256PlusPlus rng(seed);
+uniform_int_distribution<> dist(1,6);
+
 class Timer
 {
 public:
@@ -87,13 +98,12 @@ void Clear()
     #endif
 }
 
-unsigned long minuteswasted = 0;
-unsigned long hourswasted = 0;
+
 
 void drawcall(){
     guage = rollcount / totalroll;
     Clear();
-    cout << "highest: " << highest << endl << "highest roll: " << highroll << endl << "total roll: " << totalroll << endl << "total runs: " << endcount << endl << "rolls/s: " << rps << endl << "runs/s: " << cps << endl << "average roll: " << setprecision(500) << guage << endl << "total sum of rolls: " << rollcount << endl << "time wasted: " << timewasted << " seconds " << minuteswasted << " minutes " << hourswasted << " hours" << endl;
+    std::cout << "highest: " << highest << endl << "highest roll: " << highroll << endl << "total roll: " << totalroll << endl << "total runs: " << endcount << endl << "rolls/s: " << rps << endl << "runs/s: " << cps << endl << "average roll: " << setprecision(500) << guage << endl << "total sum of rolls: " << rollcount << endl << "time wasted: " << timewasted << " seconds " << minuteswasted << " minutes " << hourswasted << " hours" << endl;
     rps = 0;
     cps = 0;
     timewasted++;
@@ -110,94 +120,53 @@ void drawcall(){
 
 int main(){
     Timer timer1;
-
-    time_t now = time(0); // current time in seconds since epoch
-    tm* local_time = localtime(&now);
-
-    int hour = local_time->tm_hour;
-    int minute = local_time->tm_min;
-    int second = local_time->tm_sec;
-
-    bool running = true;
-
-    long iteration = 0;
-
-    double seconds;
-
-    bool loop = true;
-    bool timerset;
-    double timerlength;
-    double timerrun;
     Timer timer2;
-
-
-    //std::signal(SIGINT, signalHandler);
-
-    cout << "starting " << hour << ":" << minute << ":" << second << endl;
-    timer1.start();
-
     cout << "Do you want to set a timer? (y=1/n=0)";
     cin >> timerset;
     if(timerset){
-
         cout << "how long will the timer be set for? (seconds)";
-
         cin >> timerlength;
         timer2.start();
     }
-
-    while(loop){ // primary loop
-                    if(!rezero){
-                roll = dist(rng); // safe gamble
-                rollcall++; totalroll++; rps++; rollcount = rollcount + roll;
-                if(roll == 3){
-                    roll = dist(rng); // risky gamble
-                    rollcall++; totalroll++; rps++; rollcount = rollcount + roll;
-                    if(roll == 3){
-                        space++;
-                    }
-                    else{
-                        rezero = true;
-                    };
-                }
-                else{};
-            };
-
-            if(rezero){
+    timer1.start();
+    // primary loop, almost everything is placed here to maximize performance, readability be damned
+    while(loop){ 
+        roll = dist(rng); // safe gamble
+        rollcall++; totalroll++; rps++; rollcount = rollcount + roll;
+        if(roll == 3){
+            roll = dist(rng); // risky gamble
+            rollcall++; totalroll++; rps++; rollcount = rollcount + roll;
+            if(roll == 3){
+                space++;
+            }
+            else{
                 if(space > highest){
                     highest = space;
-                }
-                else{};
+                };
                 if(rollcall > highroll){
                     highroll = rollcall;
                 };
-                
+                //reset the run
                 rollcall = 0;
                 space = 0;
                 rezero = false;
+                // increment the per run counters
                 endcount++;
                 cps++;
-            
-            
-        seconds = timer1.elapsedSeconds();
-        if(seconds > 1){
-            drawcall();
-            timer1.stop();
-            timer1.start();
+                //the timer checks are placed here to reduce the amount of times they are called since I have found that getting variables from classes can signifigantly reduce performance, by up to half
+                seconds = timer1.elapsedSeconds();
+                if(seconds > 1){
+                    drawcall();
+                    timer1.stop();
+                    timer1.start();
+                };
+                if(timerset){
+                    timerrun = timer2.elapsedSeconds();
+                    if(timerrun == timerlength){
+                        loop = false;
+                    };
+                };
+            };
         };
-        if(timerset){
-            timerrun = timer2.elapsedSeconds();
-            if(timerrun == timerlength){
-                loop = false;
-            }
-        }
-            }else{};
     };
-    now = time(0); // current time in seconds since epoch
-    local_time = localtime(&now);
-
-    hour = local_time->tm_hour;
-    minute = local_time->tm_min;
-    second = local_time->tm_sec;
-    cout << "complete " << hour << ":" << minute << ":" << second << endl;
-;}
+}
